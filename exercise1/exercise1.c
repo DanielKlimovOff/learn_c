6b22413fc786173756fa13715dbf4e15ff12e7c3
DanielKlimov <danielklimovoff@gmail.com>
2021-08-08 07:09:07 +0000

    Create first exercise

---
+++ exercise1/exercise1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+    printf("|%d|\n", 555);
+    printf("|%10d|\n", 555);
+    printf("|%-10d|\n", 555);
+    printf("\n");
+    printf("Words:%9d\n", 59);
+    printf("Letters:%7d\n", 1004);
+    printf("Digits:%8d\n", 8);
+
+    return 0;
+}
+
